0fd60616df025da5e7bf5d6f8cc1d0c2e39826cc
RMS <rene.as.himself@gmail.com>
2019-02-11 13:27:15 -0600

    Implemented functional tests

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,62 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+'use strict'
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+const expect = require('chai').expect
+const MongoClient = require('mongodb')
+const fetch = require('node-fetch')
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const CONNECTION_STRING = process.env.DB //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const URL = 'https://www.alphavantage.co/query?'
+// const URL = 'https://finance.google.com/finance/info?q=NASDAQ%3a'
 
-module.exports = function (app) {
+module.exports = function(app) {
+    app.route('/api/stock-prices').get(function(req, res) {
+        const query = req.query
+        const ip = req.headers['referer'].split(',')[0] //'x-forwarded-for'
+        console.log(query)
+        console.log(ip)
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
-};
+        let stock = query.stock
+
+        if (stock === '' || stock === undefined) {
+            return res
+                .status(400)
+                .type('text')
+                .send('no stock provided')
+        }
+
+        if (Array.isArray(stock)) {
+            stock.map(item => item.toUpperCase())
+        } else {
+            stock = stock.toUpperCase()
+        }
+
+        //API information: https://github.com/AvapiDotNet/Avapi/wiki/BATCH_STOCK_QUOTES
+
+        const API_DATA = {
+            function: 'BATCH_STOCK_QUOTES',
+            symbols: stock.join(','),
+            apikey: process.env.API_KEY,
+        }
+
+        const fetchData = `${URL}function=${API_DATA.function}&symbols=${
+            API_DATA.symbols
+        }&apikey=${API_DATA.apikey}`
+
+        fetch(fetchData)
+            .then(res => res.json())
+            .then(data => console.log(data))
+
+        //  {"stockData":{"stock":"GOOG","price":"786.90","likes":1}}
+        // const stockData = {}
+
+        // return res.status(200).json(stockData)
+    })
+}
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,50 +1,245 @@
 /*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
+ *
+ *
+ *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+ *       -----[Keep the tests in the same order!]-----
+ *       (if additional are added, keep them at the very end!)
+ */
 
-var chaiHttp = require('chai-http');
-var chai = require('chai');
-var assert = chai.assert;
-var server = require('../server');
+const chaiHttp = require('chai-http')
+const chai = require('chai')
+const assert = chai.assert
+const server = require('../server')
 
-chai.use(chaiHttp);
+chai.use(chaiHttp)
 
 suite('Functional Tests', function() {
-    
     suite('GET /api/stock-prices => stockData object', function() {
-      
-      test('1 stock', function(done) {
-       chai.request(server)
-        .get('/api/stock-prices')
-        .query({stock: 'goog'})
-        .end(function(err, res){
-          
-          //complete this one too
-          
-          done();
-        });
-      });
-      
-      test('1 stock with like', function(done) {
-        
-      });
-      
-      test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
-      });
-      
-      test('2 stocks', function(done) {
-        
-      });
-      
-      test('2 stocks with like', function(done) {
-        
-      });
-      
-    });
+        test('1 stock', function(done) {
+            chai.request(server)
+                .get('/api/stock-prices')
+                .query({ stock: 'goog' })
+                .end(function(err, res) {
+                    //complete this one too
+                    assert.equal(res.status, 200)
+                    assert.isObject(
+                        res.body,
+                        'stockData respose should be object'
+                    )
+                    assert.isNotArray(
+                        res.body.stockData,
+                        'stockData response should not be an array'
+                    )
+                    assert.property(
+                        res.body,
+                        'stockData',
+                        'stockData is an object'
+                    )
+                    assert.property(
+                        res.body.stockData,
+                        'stock',
+                        'stockData has stock property'
+                    )
+                    assert.property(
+                        res.body.stockData,
+                        'price',
+                        'stockData has price property'
+                    )
+                    assert.property(
+                        res.body.stockData,
+                        'likes',
+                        'stockData has likes property'
+                    )
+                    assert.equal(res.body.stock, 'GOOG')
+                    done()
+                })
+        })
 
-});
+        test('1 stock with like', function(done) {
+            chai.request(server)
+                .get('/api/books')
+                .query({ stock: 'goog', like: true })
+                .end(function(err, res) {
+                    //complete this one too
+                    assert.equal(res.status, 200)
+                    assert.isObject(
+                        res.body,
+                        'stockData respose should be object'
+                    )
+                    assert.isNotArray(
+                        res.body.stockData,
+                        'stockData response should not be an array'
+                    )
+                    assert.property(
+                        res.body,
+                        'stockData',
+                        'stockData is an object'
+                    )
+                    assert.property(
+                        res.body.stockData,
+                        'stock',
+                        'stockData has stock property'
+                    )
+                    assert.property(
+                        res.body.stockData,
+                        'price',
+                        'stockData has price property'
+                    )
+                    assert.property(
+                        res.body.stockData,
+                        'likes',
+                        'stockData has likes property'
+                    )
+                    assert.equal(res.body.stockData.stock, 'GOOG')
+                    assert.equal(res.body.stockData.likes, 1)
+                    done()
+                })
+        })
+
+        test('1 stock with like again (ensure likes arent double counted)', function(done) {
+            chai.request(server)
+                .get('/api/stock-prices')
+                .query({ stock: 'goog', like: true })
+                .end(function(err, res) {
+                    assert.equal(res.status, 200)
+                    assert.isObject(
+                        res.body,
+                        'stockData respose should be object'
+                    )
+                    assert.isNotArray(
+                        res.body.stockData,
+                        'stockData response should not be an array'
+                    )
+                    assert.property(
+                        res.body,
+                        'stockData',
+                        'stockData is an object'
+                    )
+                    assert.property(
+                        res.body.stockData,
+                        'stock',
+                        'stockData has stock property'
+                    )
+                    assert.property(
+                        res.body.stockData,
+                        'price',
+                        'stockData has price property'
+                    )
+                    assert.property(
+                        res.body.stockData,
+                        'likes',
+                        'stockData has likes property'
+                    )
+                    assert.equal(res.body.stockData.stock, 'GOOG')
+                    assert.equal(res.body.stockData.likes, 1)
+                    done()
+                })
+        })
+
+        test('2 stocks', function(done) {
+            chai.request(server)
+                .get('/api/books')
+                .query({ stock: ['goog', 'fb'] })
+                .end((req, res) => {
+                    assert.equal(res.status, 200)
+                    assert.isArray(res.body, 'response should be array')
+                    assert.isObject(
+                        res.body.stockData[0],
+                        'response first element should be object'
+                    )
+                    assert.isObject(
+                        res.body.stockData[1],
+                        'response second element should be object'
+                    )
+                    assert.property(
+                        res.body.stockData[0],
+                        'stock',
+                        'stockData has stock property'
+                    )
+                    assert.property(
+                        res.body.stockData[0],
+                        'price',
+                        'stockData has price property'
+                    )
+                    assert.property(
+                        res.body.stockData[0],
+                        'rel_likes',
+                        'stockData has rel_likes property'
+                    )
+                    assert.property(
+                        res.body.stockData[1],
+                        'stock',
+                        'stockData has stock property'
+                    )
+                    assert.property(
+                        res.body.stockData[1],
+                        'price',
+                        'stockData has price property'
+                    )
+                    assert.property(
+                        res.body.stockData[1],
+                        'rel_likes',
+                        'stockData has rel_likes property'
+                    )
+                    assert.equal(res.body.stockData[0].stock, 'GOOG')
+                    assert.equal(res.body.stockData[0].rel_likes, 1)
+                    assert.equal(res.body.stockData[1].stock, 'FB')
+                    assert.equal(res.body.stockData[1].rel_likes, 1)
+                    done()
+                })
+        })
+
+        test('2 stocks with like', function(done) {
+            chai.request(server)
+                .get('/api/books')
+                .query({ stock: ['goog', 'fb'], like: true })
+                .end((req, res) => {
+                    assert.equal(res.status, 200)
+                    assert.isArray(res.body, 'response should be array')
+                    assert.isObject(
+                        res.body.stockData[0],
+                        'response first element should be object'
+                    )
+                    assert.isObject(
+                        res.body.stockData[1],
+                        'response second element should be object'
+                    )
+                    assert.property(
+                        res.body.stockData[0],
+                        'stock',
+                        'stockData has stock property'
+                    )
+                    assert.property(
+                        res.body.stockData[0],
+                        'price',
+                        'stockData has price property'
+                    )
+                    assert.property(
+                        res.body.stockData[0],
+                        'rel_likes',
+                        'stockData has rel_likes property'
+                    )
+                    assert.property(
+                        res.body.stockData[1],
+                        'stock',
+                        'stockData has stock property'
+                    )
+                    assert.property(
+                        res.body.stockData[1],
+                        'price',
+                        'stockData has price property'
+                    )
+                    assert.property(
+                        res.body.stockData[1],
+                        'rel_likes',
+                        'stockData has rel_likes property'
+                    )
+                    assert.equal(res.body.stockData[0].stock, 'GOOG')
+                    assert.equal(res.body.stockData[0].rel_likes, 1)
+                    assert.equal(res.body.stockData[1].stock, 'FB')
+                    assert.equal(res.body.stockData[1].rel_likes, 1)
+                    done()
+                })
+        })
+    })
+})
